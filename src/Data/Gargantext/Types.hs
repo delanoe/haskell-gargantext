da0acc7e23631af87e9b38276193497735253db8
Alexandre Delanoë <devel+git@delanoe.org>
2018-02-06 13:47:09 +0100

    Removing files.

--- src/Data/Gargantext/Types.hs
+++
@@ -1,6 +0,0 @@
-module Data.Gargantext.Types ( module Data.Gargantext.Types.Main
-                          , module Data.Gargantext.Types.Node
-                          ) where
-
-import Data.Gargantext.Types.Main
-import Data.Gargantext.Types.Node
