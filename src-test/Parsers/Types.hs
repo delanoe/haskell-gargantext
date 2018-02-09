71fb55451f91f739d5dcbfa70b8447958159c0fb
Mudada <mael.nicolas@imt.fr>
2018-02-09 14:44:22 +0100

    Added Eq instance for ZonedTime

--- src-test/Parsers/Types.hs
+++ src-test/Parsers/Types.hs
@@ -1,13 +1,18 @@
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StandaloneDeriving #-}
 
 module Parsers.Types where
 
 import Gargantext.Prelude
 
 import Test.QuickCheck
+import Test.QuickCheck.Instances()
 
 import Text.Parsec.Pos
-import Text.Parsec.Error (ParseError(..), Message(..), newErrorMessage)
+import Text.Parsec.Error (ParseError, Message(..), newErrorMessage)
+import Data.Time.LocalTime (ZonedTime (..))
+import Data.Eq (Eq(..))
+deriving instance Eq ZonedTime
 
 instance Arbitrary Message where
   arbitrary = do
