cf12f83c16c4429fe9138797b1c91059ce05f8d2
Mudada <mael.nicolas@imt.fr>
2018-03-02 11:24:10 +0100

    add lose precision

--- src-test/Parsers/Date.hs
+++ src-test/Parsers/Date.hs
@@ -8,6 +8,8 @@ import Test.Hspec
 import Test.QuickCheck
 import Parsers.Types
 
+import Control.Applicative ((<*>))
+import Data.Tuple (uncurry)
 import Data.Either (Either(..))
 import Data.Time (ZonedTime(..))
 import Data.Text (pack, Text)
@@ -18,11 +20,15 @@ import Gargantext.Parsers.Date (fromRFC3339)
 
 fromRFC3339Inv ::  Either ParseError ZonedTime -> Text
 fromRFC3339Inv (Right z) = toRFC3339 z
-fromRFC3339Inv (Left pe) = pack $ show pe
+fromRFC3339Inv (Left pe) = panic . pack $ show pe
 
-{-testFromRFC3339 :: IO ()
+testFromRFC3339 :: IO ()
 testFromRFC3339 = hspec $ do
   describe "Test fromRFC3339: " $ do
     it "is the inverse of Duckling's toRFC3339" $ property $
-      \x -> (fromRFC3339 . fromRFC3339Inv) x == (x :: Either ParseError ZonedTime)
---}
+      ((==) <*> (fromRFC3339 . fromRFC3339Inv)) . Right . looseZonedTimePrecision
+
+      -- \x -> uncurry (==) $ (,) <*> (fromRFC3339 . fromRFC3339Inv) $ Right $ looseZonedTimePrecision x
+
+      -- \x -> let e = Right x :: Either ParseError ZonedTime
+      --       in fmap looseZonedTimePrecision e == (fromRFC3339 . fromRFC3339Inv ) (fmap looseZonedTimePrecision e)
--- src-test/Parsers/Types.hs
+++ src-test/Parsers/Types.hs
@@ -4,15 +4,34 @@
 module Parsers.Types where
 
 import Gargantext.Prelude
+import Prelude (floor, fromIntegral)
 
 import Test.QuickCheck
-import Test.QuickCheck.Instances()
+import Test.QuickCheck.Instances ()
 
 import Text.Parsec.Pos
 import Text.Parsec.Error (ParseError, Message(..), newErrorMessage)
-import Data.Time.LocalTime (ZonedTime (..))
+import Data.Time.LocalTime (ZonedTime (..), TimeZone (..), TimeOfDay(..), LocalTime(..))
 import Data.Eq (Eq(..))
+import Data.Either (Either(..))
+
 deriving instance Eq ZonedTime
+
+looseTimeOfDayPrecision :: TimeOfDay -> TimeOfDay
+looseTimeOfDayPrecision (TimeOfDay h m s) = TimeOfDay h m 0
+
+looseLocalTimePrecision :: LocalTime -> LocalTime
+looseLocalTimePrecision (LocalTime ld ltd) = LocalTime ld $ looseTimeOfDayPrecision ltd
+
+looseTimeZonePrecision :: TimeZone -> TimeZone
+looseTimeZonePrecision (TimeZone zm _ _) = TimeZone zm False "CET"
+
+looseZonedTimePrecision :: ZonedTime -> ZonedTime
+looseZonedTimePrecision (ZonedTime lt tz) = ZonedTime (looseLocalTimePrecision lt) $ looseTimeZonePrecision tz
+
+loosePrecisionEitherPEZT :: Either ParseError ZonedTime -> Either ParseError ZonedTime
+loosePrecisionEitherPEZT (Right zt) = Right $ looseZonedTimePrecision zt
+loosePrecisionEitherPEZT pe = pe
 
 instance Arbitrary Message where
   arbitrary = do
