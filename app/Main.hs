1f7c076866fe213ffecaf77bb88571d934bcae2a
Alexandre Delanoë <devel+git@delanoe.org>
2018-03-04 00:19:56 +0100

    [APP] command line options added.

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,63 @@
+{-|
+Module      : Main.hs
+Description : Gargantext starter
+Copyright   : (c) CNRS, 2017-Present
+License     : AGPL + CECILL v3
+Maintainer  : team@gargantext.org
+Stability   : experimental
+Portability : POSIX
+-}
+
+
 {-# LANGUAGE NoImplicitPrelude #-}
+
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
+import Options.Generic
+import Data.Text (unpack)
+
 import Gargantext.Prelude
-import Gargantext.API (startGargantext)
-import Text.Read (read)
-import System.Environment (getArgs)
+import Gargantext.API (startGargantext, startGargantextMock)
+
+------------------------------------------------------
+
+data Mode = Dev | Mock | Prod 
+        deriving (Show, Read, Generic)
+instance ParseRecord Mode
+instance ParseField Mode
+instance ParseFields Mode
+
+data MyOptions = MyOptions { port    :: Maybe Int
+                           , iniFile :: Maybe Text
+                           , mode    :: Maybe Mode
+                           } 
+        deriving (Generic, Show)
+
+instance ParseRecord MyOptions
+
+
 
 main :: IO ()
 main = do 
-  (port:iniFile:_) <- getArgs
-  startGargantext (read port :: Int) iniFile
+    MyOptions myPort myIniFile myMode <- getRecord 
+            "Gargantext: collaborative platform for text-mining"
+    
+    let myPort' = case myPort of
+            Just p  -> p
+            Nothing -> 8008
+
+
+    let start = case myMode of
+            --Nothing   -> startGargantext myPort' (unpack myIniFile')
+            Just Prod -> startGargantext myPort' (unpack myIniFile')
+                    where
+                        myIniFile' = case myIniFile of
+                                       Nothing -> panic "Need gargantext.ini file"
+                                       Just i  -> i
+            Just Mock -> startGargantextMock myPort'
+            _         -> startGargantextMock myPort'
+    
+    start
