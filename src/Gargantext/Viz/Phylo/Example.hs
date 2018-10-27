18120559a39d14c960034d2c68e58caa38d8dc16
Alexandre Delanoë <devel+git@delanoe.org>
2018-10-27 21:31:55 +0200

    [PHYLO] some types and explicit functions, further work needed. Metrics will be added soon.

--- src/Gargantext/Viz/Phylo/Example.hs
+++ src/Gargantext/Viz/Phylo/Example.hs
@@ -11,9 +11,14 @@ Portability : POSIX
 -- Exemple de phylomemie
 -- French without accents
 
--- TODO: chevauchement
+
+TODO:
+- split the functions : RAW -> Document -> Ngrams
+
 -- reverse history: antechronologique
--- data with occurrence de chaque terme
+-- metrics support
+
+
 -}
 
 {-# LANGUAGE NoImplicitPrelude #-}
@@ -36,58 +41,74 @@ import Gargantext.Text.Terms.Mono (monoTexts)
 import Gargantext.Prelude
 
 ------------------------------------------------------------------------
-type History = [Event]
-data Event = Event {date:: Double, text :: Text}
+-- | For this example Date is simply an Integer
+type Date     = Int
+type Corpus   = [Document]
+data Document =
+     Document
+     { date :: Date
+     , text :: Text
+     }
   deriving (Show)
 
-
-type MapList = [Text]
+type Ngrams  = Text
+type MapList = [Ngrams]
 type PeriodeSize = Int
 -- data Periodes b a = Map (b,b) a
 
 ------------------------------------------------------------------------
+
+fis :: Map (Date, Date) [Document]
+    -> Map (Date, Date) (Map (Set Ngrams) Int)
+fis = phylo (words . text)
+
+-- | TODO: parameters has to be checked
 -- | TODO FIS on monotexts
-phyloFIS :: Map (Double, Double) [Event] -> Map (Double, Double) (Map (Set Text) Int)
-phyloFIS = DM.map (\n -> fisWithSizePolyMap (Segment 1 20) 1 (map (words . text) n))
+phylo :: (Document -> [Ngrams])
+      -> Map (Date, Date) [Document]
+      -> Map (Date, Date) (Map (Set Ngrams) Int)
+phylo f = DM.map (\d -> fisWithSizePolyMap (Segment 1 20) 1 (map f d))
 
-history :: History
-history = cleanHistory  mapList phyloCorpus
+phyloTerms :: Map (Date, Date) [Document]
+phyloTerms = toPeriodes date 5 3 $ cleanCorpus mapList phyloCorpus
 
-phyloTerms :: Map (Double, Double) [Event]
-phyloTerms = toPeriodes date 5 3 $ cleanHistory  mapList phyloCorpus
 ------------------------------------------------------------------------
-toPeriodes :: (Ord date, Enum date) => (event -> date)
-     -> Grain -> Step -> [event] -> Map (date, date) [event]
-toPeriodes _ _ _ [] = panic "Empty history can not have any periods"
+toPeriodes :: (Ord date, Enum date) => (doc -> date)
+     -> Grain -> Step -> [doc] -> Map (date, date) [doc]
+toPeriodes _ _ _ [] = panic "Empty corpus can not have any periods"
 toPeriodes f g s es = DM.fromList $ zip hs $ map (inPeriode f es) hs
   where
     hs = steps g s $ both f (DL.head es, DL.last es)
 
-inPeriode :: Ord b => (t -> b) -> [t] -> (b, b) -> [t]
-inPeriode f h (start,end) =
-  fst $ DL.partition (\d -> f d >= start && f d <= end) h
-------------------------------------------------------------------------
--- | Steps of linear and homogenous time of integer as granalurity measure
--- chunkAlong deals with [] case (not error with head and last then)
-steps :: (Eq date, Enum date) => Grain -> Step -> (date, date) -> [(date, date)]
-steps s o (start,end) = map (\l -> (DL.head l, DL.last l))
-                      $ chunkAlong s o [start .. end]
-------------------------------------------------------------------------
-cleanHistory :: MapList -> History -> History
-cleanHistory ml = map (\(Event d t) -> Event d (unwords $ filter (\x -> elem  x ml) $ monoTexts t))
+    inPeriode :: Ord b => (t -> b) -> [t] -> (b, b) -> [t]
+    inPeriode f' h (start,end) =
+      fst $ DL.partition (\d -> f' d >= start && f' d <= end) h
+    ------------------------------------------------------------------------
+    -- | Steps of linear and homogenous time of integer as granalurity measure
+    -- chunkAlong deals with [] case (not error with head and last then)
+    steps :: (Eq date, Enum date) => Grain -> Step -> (date, date) -> [(date, date)]
+    steps s' o' (start,end) = map (\l -> (DL.head l, DL.last l))
+                          $ chunkAlong s' o' [start .. end]
+    ------------------------------------------------------------------------
 
-mapList :: [Text]
+cleanCorpus :: MapList -> Corpus -> Corpus
+cleanCorpus ml = map (\(Document d t) -> Document d (unwords $ filter (\x -> elem  x ml) $ monoTexts t))
+
+mapList :: [Ngrams]
 mapList = map toLower actants
 
-actants :: [Text]
+actants :: [Ngrams]
 actants = [ "Cleopatre"   , "Ptolemee", "Ptolemee-XIII", "Ptolemee-XIV"
           , "Marc-Antoine", "Cesar"   , "Antoine"      , "Octave"  , "Rome"
           , "Alexandrie"  , "Auguste" , "Pompee"       , "Cassius" , "Brutus"]
 
-phyloCorpus :: History
-phyloCorpus = map (\(d,t) -> Event d t) corpus
+phyloCorpus :: Corpus
+phyloCorpus = map (\(d,t) -> Document d t) corpus
+
+
+
 
 ------------------------------------------------------------------------
-corpus :: [(Double, Text)]
+corpus :: [(Date, Text)]
 corpus = DL.sortOn fst [ (-51,"Cleopatre règne sur l’egypte entre 51 et 30 av. J.-C. avec ses frères-epoux Ptolemee-XIII et Ptolemee-XIV, puis aux côtes du general romain Marc-Antoine. Elle est celèbre pour avoir ete la compagne de Jules Cesar puis d'Antoine, avec lesquels elle a eu plusieurs enfants. Partie prenante dans la guerre civile opposant Antoine à Octave, elle est vaincue à la bataille d'Actium en 31 av. J.-C. Sa defaite va permettre aux Romains de mener à bien la conquête de l’egypte, evenement qui marquera la fin de l'epoque hellenistique."), (-40,"Il existe relativement peu d'informations sur son sejour à Rome, au lendemain de l'assassinat de Cesar, ou sur la periode passee à Alexandrie durant l'absence d'Antoine, entre -40 et -37."), (-48,"L'historiographie antique lui est globalement defavorable car inspiree par son vainqueur, l'empereur Auguste, et par son entourage, dont l'interêt est de la noircir, afin d'en faire l'adversaire malfaisant de Rome et le mauvais genie d'Antoine. On observe par ailleurs que Cesar ne fait aucune mention de sa liaison avec elle dans les Commentaires sur la Guerre civile"), (-69,"Cleopatre est nee au cours de l'hiver -69/-686 probablement à Alexandrie."), (-48,"Pompee a en effet ete le protecteur de Ptolemee XII, le père de Cleopatre et de Ptolemee-XIII dont il se considère comme le tuteur."), (-48,"Ptolemee-XIII et Cleopatre auraient d'ailleurs aide Pompee par l'envoi d'une flotte de soixante navires."), (-48,"Mais le jeune roi Ptolemee-XIII et ses conseillers jugent sa cause perdue et pensent s'attirer les bonnes graces du vainqueur en le faisant assassiner à peine a-t-il pose le pied sur le sol egyptien, près de Peluse, le 30 juillet 48 av. J.-C., sous les yeux de son entourage."), (-48,"Cesar fait enterrer la tête de Pompee dans le bosquet de Nemesis en bordure du mur est de l'enceinte d'Alexandrie. Pour autant la mort de Pompee est une aubaine pour Cesar qui tente par ailleurs de profiter des querelles dynastiques pour annexer l’egypte."), (-48,"Il est difficile de se prononcer clairement sur les raisons qui ont pousse Cesar à s'attarder à Alexandrie. Il y a des raisons politiques, mais aussi des raisons plus sentimentales (Cleopatre ?). Il tente d'abord d'obtenir le remboursement de dettes que Ptolemee XII"), (-46,"Les deux souverains sont convoques par Cesar au palais royal d'Alexandrie. Ptolemee-XIII s'y rend après diverses tergiversations ainsi que Cleopatre."), (-47,"A Rome, Cleopatre epouse alors un autre de ses frères cadets, à Alexandrie, Ptolemee-XIV, sur l'injonction de Jules Cesar"), (-46,"Cesar a-t-il comme objectif de montrer ce qu'il en coûte de se revolter contre Rome en faisant figurer dans son triomphe la sœur de Cleopatre et de Ptolemee-XIV, Arsinoe, qui s'est fait reconnaître reine par les troupes de Ptolemee-XIII ?"), (-44,"Au debut de l'annee -44, Cesar est assassine par Brutus. Profitant de la situation confuse qui s'ensuit, Cleopatre quitte alors Rome à la mi-avril, faisant escale en Grèce. Elle parvient à Alexandrie en juillet -44."), (-44,"La guerre que se livrent les assassins de Cesar, Cassius et Brutus et ses heritiers, Octave et Marc-Antoine, oblige Cleopatre à des contorsions diplomatiques."), (-41,"Nous ignorons depuis quand Cleopatre, agee de 29 ans en -41, et Marc-Antoine, qui a une quarantaine d'annees, se connaissent. Marc-Antoine est l'un des officiers qui ont participe au retablissement de Ptolemee XII.  Il est plus vraisemblable qu'ils se soient frequentes lors du sejour à Rome de Cleopatre."), (-42,"Brutus tient la Grèce tandis que Cassius s'installe en Syrie. Le gouverneur de Cleopatre à Chypre, Serapion, vient en aide à Cassius."), (-42,"Cassius aurait envisage de s'emparer d'Alexandrie quand le 'debarquement' en Grèce d'Antoine et d'Octave l'oblige à renoncer à ses projets")]
 
