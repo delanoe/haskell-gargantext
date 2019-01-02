c2dbcba75d6728ad39e31fdb5422bba6ae32402f
Alexandre Delanoë <devel+git@delanoe.org>
2019-01-02 15:01:37 +0100

    [TEXT][Parsers][Isidore] working query.

--- src/Gargantext/Text/Parsers/Isidore.hs
+++ src/Gargantext/Text/Parsers/Isidore.hs
@@ -22,9 +22,9 @@ route = "https://isidore.science/sparql/"
 selectQueryRaw' :: String -> String -> IO (Response ByteString)
 selectQueryRaw' uri q = getWith opts uri
   where
-    opts = defaults & header "Accept" .~ ["application/sparql-results+xml"]
+    opts = defaults & header "Accept"     .~ ["application/sparql-results+xml"]
                     & header "User-Agent" .~ ["gargantext-hsparql-client"]
-                    & param "query" .~ [Data.Text.pack q]
+                    & param  "query"      .~ [Data.Text.pack q]
 
 isidoreGet :: Text -> IO ByteString
 isidoreGet q = do
@@ -38,25 +38,47 @@ isidoreGet q = do
 
 simpleSelect :: Text -> Query SelectQuery
 simpleSelect q = do
-  isidore <- prefix "isidore" (iriRef "http://www.rechercheisidore.fr/class")
+  -- See Predefined Namespace Prefixes:
+  -- https://isidore.science/sparql?nsdecl
+  isidore <- prefix "isidore" (iriRef "http://www.rechercheisidore.fr/class/")
   rdf     <- prefix "rdf"     (iriRef "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
-  dcterms <- prefix "dcterms" (iriRef "http://purl.org/dc/terms")
-  dc      <- prefix "dc"      (iriRef "http://purl.org/dc")
+  dcterms <- prefix "dcterms" (iriRef "http://purl.org/dc/terms/")
+  dc      <- prefix "dc"      (iriRef "http://purl.org/dc/elements/1.1/")
+  ore     <- prefix "ore"    (iriRef "http://www.openarchives.org/ore/terms/")
+  bif     <- prefix "bif"    (iriRef "bif:")
 
-  doc      <- var
+  link     <- var
   title    <- var
-  --date     <- var
+  date     <- var
   abstract <- var
-  --source   <- var
+  authors  <- var
+  source   <- var
+  lang     <- var
+  publisher <- var
+  agg       <- var
 
-  triple_ doc (rdf .:. "type")      (isidore .:. "BibliographicalResource")
-  triple_ doc (dcterms .:. "title")  title
-  --triple doc (dcterms .:. "date")        date
-  --triple doc (dcterms .:. "source")      source
-  triple doc (dc      .:. "description") abstract
+  indexT  <- var
+
+  triple_ link (rdf     .:. "type")     (isidore .:. "BibliographicalResource")
+  triple_ link (dcterms .:. "title")    title
+  triple_ link (dcterms .:. "date")     date
+  triple_ link (dcterms .:. "creator")  authors
+  triple_ link (dcterms .:. "language") lang
+  triple_ link (dcterms .:. "source")   source
+
+  triple_ link (ore .:. "isAggregatedBy") agg
+  --triple_ agg (dcterms .:. "title") title
+
+
+  optional $ triple_ link (dcterms .:. "publisher") publisher
+  optional $ triple_ link (dc      .:. "description") abstract
 
   --filterExpr $ (.||.) (contains title q) (contains abstract q)
-  filterExpr_ (contains title q) -- (contains abstract q)
-  limit_ 3
 
-  selectVars [title]
+  --triple_ indexT (bif .:. "contains") title
+
+  --filterExpr_ (contains indexT q) -- (contains abstract q)
+  orderNextDesc date
+  limit_ 10
+  distinct_
+  selectVars [link, date, authors, source, title, lang, publisher, abstract]
