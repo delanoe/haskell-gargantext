87fcabcc18f162420817aedca50eb83d337e5696
Alexandre Delanoë <devel+git@delanoe.org>
2019-04-05 17:25:07 +0200

    [PHYLO.API] missing file.

---
+++ src/Gargantext/Viz/Phylo/API.hs
@@ -0,0 +1,36 @@
+{-|
+Module      : Gargantext.Viz.Phylo.Tools
+Description : Phylomemy Tools to build/manage it
+Copyright   : (c) CNRS, 2017-Present
+License     : AGPL + CECILL v3
+Maintainer  : team@gargantext.org
+Stability   : experimental
+Portability : POSIX
+
+-}
+
+{-# LANGUAGE NoImplicitPrelude #-}
+
+
+module Gargantext.Viz.Phylo.API
+  where
+
+--{-
+import Gargantext.Database.Types.Node (PhyloId, ListId, CorpusId)
+import Gargantext.Prelude
+import Gargantext.Viz.Phylo
+import Gargantext.Viz.Phylo.Example
+
+getPhylo :: PhyloId -> PhyloQueryView -> PhyloView
+getPhylo _phyloId _phyloQueryView = phyloView
+
+postPhylo :: CorpusId -> Maybe ListId -> PhyloQueryBuild -> Phylo
+postPhylo = undefined
+
+putPhylo :: PhyloId -> Maybe ListId -> PhyloQueryBuild -> Phylo
+putPhylo = undefined
+
+deletePhylo :: PhyloId -> IO ()
+deletePhylo = undefined
+
+--}
