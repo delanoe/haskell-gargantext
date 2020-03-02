c265bb08969ff2a787fc9527296181d577ccc0dc
justinwoo <moomoowoo@gmail.com>
2020-03-02 19:26:12 +0200

    update build-shell.nix and add gfortran as extra-libraries
    this makes `stack build --nix` work without extra system dependencies

--- build-shell.nix
+++ build-shell.nix
@@ -1,24 +1,32 @@
-{ghc}:
-with (import ./pinned.nix {});
-
-haskell.lib.buildStackProject {
-  inherit ghc;
-  name = "gargantext";
-  buildInputs = [
-    docker-compose
-    blas
+{ ghc
+, pkgs ? import ./pinned.nix {}
+}:
+let
+  buildInputs = with pkgs; [
     bzip2
-    #gfortran
-    gfortran.cc.lib
-    glibc
     gmp
     gsl
     igraph
     liblapack
+    lzma
     pcre
     postgresql
-    #stack
     xz
     zlib
+    blas
+    gfortran7
+    gfortran7.cc.lib
   ];
+
+  libraryPaths = pkgs.lib.makeLibraryPath buildInputs;
+in
+pkgs.haskell.lib.buildStackProject rec {
+  inherit ghc;
+  inherit buildInputs;
+  name = "gargantext";
+
+  shellHook = ''
+    export LD_LIBRARY_PATH="${libraryPaths}"
+    export LIBRARY_PATH="${libraryPaths}"
+  '';
 }
