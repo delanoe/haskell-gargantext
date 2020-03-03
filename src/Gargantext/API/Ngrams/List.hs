03d73fcaee2b7e075c94f1f450131c7eb866d643
Przemek Kaminski <pk@intrepidus.pl>
2020-03-03 12:02:18 +0100

    [list] add HTML list get endpoint

--- src/Gargantext/API/Ngrams/List.hs
+++ src/Gargantext/API/Ngrams/List.hs
@@ -12,6 +12,8 @@ Portability : POSIX
 {-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RankNTypes        #-}
@@ -21,30 +23,50 @@ Portability : POSIX
 module Gargantext.API.Ngrams.List
   where
 
-import Gargantext.Prelude
-import Gargantext.API.Ngrams
-import Servant
+import Data.Aeson
+-- import qualified Data.ByteString.Lazy as BSL
 import Data.List (zip)
 import Data.Map (Map, toList, fromList)
-import Gargantext.Database.Types.Node
-import Gargantext.Database.Schema.Ngrams (NgramsType(..), ngramsTypes)
-import Gargantext.Database.Flow (FlowCmdM)
+-- import qualified Data.Text as T
+-- import qualified Data.Text.Encoding as TE
+import Network.HTTP.Media ((//), (/:))
+import Servant
+
+import Gargantext.Prelude
+import Gargantext.API.Ngrams
 import Gargantext.API.Types (GargServer)
-import Gargantext.API.Ngrams (putListNgrams')
+import Gargantext.Database.Flow (FlowCmdM)
+import Gargantext.Database.Schema.Ngrams (NgramsType(..), ngramsTypes)
+import Gargantext.Database.Types.Node
 
 type NgramsList = (Map NgramsType (Versioned NgramsTableMap))
 
+data HTML
+instance Accept HTML where
+  contentType _ = "text" // "html" /: ("charset", "utf-8")
+instance ToJSON a => MimeRender HTML a where
+  mimeRender _ = encode
+
 type API = Get '[JSON] NgramsList
       :<|> ReqBody '[JSON] NgramsList :> Put '[JSON] Bool
+      :<|> Get '[HTML] NgramsList
 
 api :: ListId -> GargServer API
-api l = get l :<|> put l
+api l = get l :<|> put l :<|> get l
 
-get :: RepoCmdM env err m 
+get :: RepoCmdM env err m
     => ListId -> m NgramsList
 get lId = fromList
        <$> zip ngramsTypes
        <$> mapM (getNgramsTableMap lId) ngramsTypes
+
+getHtml :: RepoCmdM env err m
+        => ListId -> m NgramsList
+getHtml lId = do
+  lst <- get lId
+  return lst
+  --return $ TE.decodeUtf8 $ BSL.toStrict $ encode lst
+
 
 -- TODO : purge list
 put :: FlowCmdM env err m
