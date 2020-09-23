74baff146296f3c1bcdff3280f15cbf2c3f2f2ce
Alexandre Delanoë <devel+git@delanoe.org>
2020-09-23 10:04:00 +0200

    [Update] mail with body type

--- src/Gargantext/Database/Action/User.hs
+++ src/Gargantext/Database/Action/User.hs
@@ -33,23 +33,32 @@ mkUsers address us = do
   us' <- liftBase    $ mapM toUserHash us
   r   <- insertUsers $ map toUserWrite us'
   _   <- mapM getOrMkRoot $ map (\u -> UserName (_nu_username u)) us
-  _   <- liftBase    $ mapM (mail address) us
+  _   <- liftBase    $ mapM (mail Invitation address) us
   pure r
 ------------------------------------------------------------------------
 updateUser :: HasNodeError err => Text -> NewUser GargPassword -> Cmd err Int64
 updateUser address u = do
   u' <- liftBase   $ toUserHash   u
   n  <- updateUserDB $ toUserWrite  u'
-  _  <- liftBase   $ mail address u
+  _  <- liftBase   $ mail Update address u
   pure n
 
 ------------------------------------------------------------------------
+data Mail = Invitation
+          | Update
+
+
 -- TODO gargantext.ini config
-mail :: Text -> NewUser GargPassword -> IO ()
-mail address nu@(NewUser u m _) = gargMail (GargMail m (Just u) subject body)
+mail :: Mail -> Text -> NewUser GargPassword -> IO ()
+mail mtype address nu@(NewUser u m _) = gargMail (GargMail m (Just u) subject body)
   where
     subject = "[Your Garg Account]"
-    body    = logInstructions address nu
+    body    = bodyWith mtype address nu
+
+bodyWith :: Mail -> Text -> NewUser GargPassword -> Text
+bodyWith Invitation add nu = logInstructions    add nu
+bodyWith Update     add nu = updateInstructions add nu
+
 
 -- TODO put this in a configurable file (path in gargantext.ini)
 logInstructions :: Text -> NewUser GargPassword -> Text
@@ -73,6 +82,30 @@ logInstructions address (NewUser u _ (GargPassword p)) =
           , "-- "
           , "The Gargantext Team (CNRS)"
           ]
+
+updateInstructions :: Text -> NewUser GargPassword -> Text
+updateInstructions address (NewUser u _ (GargPassword p)) =
+  unlines [ "Hello"
+          , "Your account have been updated on the GarganText platform!"
+          , ""
+          , "You can log on to: "  <> address
+          , "Your login is: "      <> u
+          , "Your password is: "   <> p
+          , ""
+          , "As reminder, please read the full terms of use on:"
+          , "https://gitlab.iscpif.fr/humanities/tofu/tree/master"
+          , ""
+          , "Your feedback is always valuable for further development"
+          , "of the platform, do not hesitate to contact us and"
+          , "to contribute on our forum:"
+          , "     https://discourse.iscpif.fr/c/gargantext"
+          , ""
+          , "With our best regards,"
+          , "-- "
+          , "The Gargantext Team (CNRS)"
+          ]
+
+
 
 ------------------------------------------------------------------------
 rmUser :: HasNodeError err => User -> Cmd err Int64
