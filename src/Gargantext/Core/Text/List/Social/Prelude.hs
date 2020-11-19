aa89001d8ddd0f59f692828fa38569d46145bc0d
Alexandre Delanoë <devel+git@delanoe.org>
2020-11-19 17:40:19 +0100

    [CLEAN] before adding Continuation FlowList type

--- src/Gargantext/Core/Text/List/Social/Prelude.hs
+++ src/Gargantext/Core/Text/List/Social/Prelude.hs
@@ -18,10 +18,16 @@ Portability : POSIX
 module Gargantext.Core.Text.List.Social.Prelude
   where
 
+import Data.Semigroup (Semigroup(..))
 import Data.Map (Map)
+import Data.Maybe (fromMaybe)
+import Data.Set (Set)
 import Data.Text (Text)
+import Gargantext.Core.Types.Main
 import Gargantext.Prelude
 import qualified Data.Map   as Map
+import qualified Data.Set   as Set
+
 
 ------------------------------------------------------------------------
 -- | Tools to inherit groupings
@@ -56,3 +62,38 @@ hasParent t m = case Map.lookup t m of
 ------------------------------------------------------------------------
 keyWithMaxValue :: Map a b -> Maybe a
 keyWithMaxValue m = (fst . fst) <$> Map.maxViewWithKey m
+
+
+
+------------------------------------------------------------------------
+-- | Tools TODO clean it (some need to be removed)
+------------------------------------------------------------------------
+termsByList :: ListType -> (Map (Maybe ListType) (Set Text)) -> Set Text
+termsByList CandidateTerm m = Set.unions
+                          $ map (\lt -> fromMaybe Set.empty $ Map.lookup lt m)
+                          [ Nothing, Just CandidateTerm ]
+termsByList l m =
+  fromMaybe Set.empty $ Map.lookup (Just l) m
+
+------------------------------------------------------------------------
+unions :: (Ord a, Semigroup a, Semigroup b, Ord b)
+      => [Map a (Set b)] -> Map a (Set b)
+unions = invertBack . Map.unionsWith (<>) . map invertForw
+
+invertForw :: (Ord b, Semigroup a) => Map a (Set b) -> Map b a
+invertForw = Map.unionsWith (<>)
+           . (map (\(k,sets) -> Map.fromSet (\_ -> k) sets))
+           . Map.toList
+
+invertBack :: (Ord a, Ord b) => Map b a -> Map a (Set b)
+invertBack = Map.fromListWith (<>)
+           . (map (\(b,a) -> (a, Set.singleton b)))
+           .  Map.toList
+
+unions_test :: Map ListType (Set Text)
+unions_test = unions [m1, m2]
+  where
+    m1 = Map.fromList [ (StopTerm     , Set.singleton "Candidate")]
+    m2 = Map.fromList [ (CandidateTerm, Set.singleton "Candidate")
+                      , (MapTerm      , Set.singleton "Candidate")
+                      ]
