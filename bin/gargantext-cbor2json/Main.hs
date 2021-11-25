8484ca0fe0243b2ec068dacf6b64021b6bf88ca9
Alexandre Delanoë <devel+git@delanoe.org>
2021-11-25 16:08:38 +0100

    [FIX] cbor2json

--- bin/gargantext-cbor2json/Main.hs
+++ bin/gargantext-cbor2json/Main.hs
@@ -1,9 +1,14 @@
-import Prelude (IO, id, (.))
+
+import System.Environment (getArgs)
+import Prelude (IO, id, (.), ($))
 import Data.Aeson (encode)
 import Codec.Serialise (deserialise)
 import qualified Data.ByteString.Lazy as L
-
-import Gargantext.API.Ngrams.Types (NgramsRepo)
+import Gargantext.Core.NodeStory (NodeListStory)
 
 main :: IO ()
-main = L.interact (encode . (id :: NgramsRepo -> NgramsRepo) . deserialise)
+main = do
+  [inFile,outFile] <- getArgs
+  inData <- L.readFile inFile
+  let outData = (encode . (id :: NodeListStory -> NodeListStory) . deserialise) inData
+  L.writeFile outData
