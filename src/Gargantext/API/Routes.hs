ea17dccbbc920aeb3733f2b7f6a479ab69fd994d
Alexandre Delanoë <devel+git@delanoe.org>
2022-01-12 16:35:58 +0100

    [FIX] Routes (merge with Document export)

--- src/Gargantext/API/Routes.hs
+++ src/Gargantext/API/Routes.hs
@@ -9,20 +9,15 @@ Portability : POSIX
 
 -}
 
-
-
 {-# LANGUAGE ConstraintKinds      #-}
 {-# LANGUAGE TypeOperators        #-}
 {-# LANGUAGE KindSignatures       #-}
 {-# LANGUAGE ScopedTypeVariables  #-}
 {-# LANGUAGE TypeFamilies         #-}
 
----------------------------------------------------------------------
 module Gargantext.API.Routes
       where
----------------------------------------------------------------------
 
--- import qualified Gargantext.API.Search as Search
 import Control.Concurrent (threadDelay)
 import Control.Lens (view)
 import Data.Text (Text)
@@ -33,26 +28,14 @@ import Servant.Auth.Swagger ()
 import Servant.Job.Async
 import Servant.Swagger.UI
 
-{-
-import qualified Gargantext.API.Ngrams.List              as List
-import qualified Gargantext.API.Node.Contact             as Contact
-import qualified Gargantext.API.Node.Corpus.Annuaire     as Annuaire
-import qualified Gargantext.API.Node.Corpus.Export       as CorpusExport
-import qualified Gargantext.API.Node.Corpus.Export.Types as CorpusExport
-import qualified Gargantext.API.Node.Corpus.New          as New
-import qualified Gargantext.API.Node.Document.Export       as DocumentExport
-import qualified Gargantext.API.Node.Document.Export.Types as DocumentExport
-import qualified Gargantext.API.Public                   as Public
-import Gargantext.API.Admin.Auth.Types (AuthRequest, AuthResponse, AuthenticatedUser(..), PathId(..))
--}
 import Gargantext.API.Admin.Auth (withAccess)
 import Gargantext.API.Admin.Auth.Types (AuthRequest, AuthResponse, AuthenticatedUser(..), PathId(..))
 import Gargantext.API.Admin.FrontEnd (FrontEndAPI)
+import Gargantext.API.Context
 import Gargantext.API.Count  (CountAPI, count, Query)
 import Gargantext.API.Job (jobLogInit)
 import Gargantext.API.Ngrams (TableNgramsApi, apiNgramsTableDoc)
 import Gargantext.API.Node
-import Gargantext.API.Context
 import Gargantext.API.Prelude
 import Gargantext.Core.Types.Individu (User(..))
 import Gargantext.Core.Viz.Graph.API
@@ -61,14 +44,16 @@ import Gargantext.Database.Admin.Types.Node
 import Gargantext.Database.Prelude (HasConfig(..))
 import Gargantext.Prelude
 import Gargantext.Prelude.Config (gc_max_docs_scrapers)
-import qualified Gargantext.API.GraphQL as GraphQL
-import qualified Gargantext.API.Ngrams.List              as List
-import qualified Gargantext.API.Node.Contact             as Contact
-import qualified Gargantext.API.Node.Corpus.Annuaire     as Annuaire
-import qualified Gargantext.API.Node.Corpus.Export       as Export
-import qualified Gargantext.API.Node.Corpus.Export.Types as Export
-import qualified Gargantext.API.Node.Corpus.New          as New
-import qualified Gargantext.API.Public                   as Public
+import qualified Gargantext.API.GraphQL                    as GraphQL
+import qualified Gargantext.API.Ngrams.List                as List
+import qualified Gargantext.API.Node.Contact               as Contact
+import qualified Gargantext.API.Node.Corpus.Annuaire       as Annuaire
+import qualified Gargantext.API.Node.Corpus.Export         as CorpusExport
+import qualified Gargantext.API.Node.Corpus.Export.Types   as CorpusExport
+import qualified Gargantext.API.Node.Corpus.New            as New
+import qualified Gargantext.API.Node.Document.Export       as DocumentExport
+import qualified Gargantext.API.Node.Document.Export.Types as DocumentExport
+import qualified Gargantext.API.Public                     as Public
 
 
 type GargAPI = "api" :> Summary "API " :> GargAPIVersion
