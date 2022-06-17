565c34d4f68b8e3e6aa2a0d1c005c33d9914dd69
Alp Mestanogullari <alp@well-typed.com>
2022-06-17 16:00:25 +0200

    forgot a file

---
+++ src/Gargantext/Core/Viz/Graph/Tools/Infomap.hs
@@ -0,0 +1,15 @@
+module Gargantext.Core.Viz.Graph.Tools.Infomap where
+
+import Data.Map (Map)
+import Graph.Types
+import Prelude
+
+import qualified Data.Graph.Infomap          as I
+import qualified Data.Graph.Infomap.Internal as I
+
+
+infomap :: String -> Map (Int, Int) Double -> IO [ClusterNode]
+infomap infomapCfg gr = map mkClustNode <$> I.infomap infomapCfg gr
+
+  where mkClustNode (I.CNode nid cid) =
+          ClusterNode (fromIntegral nid) (fromIntegral cid)
