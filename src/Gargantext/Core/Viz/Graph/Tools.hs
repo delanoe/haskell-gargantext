b98ddbfc22f184220a44939c0a8045fe30bc6e84
Alexandre Delanoë <devel+git@delanoe.org>
2022-09-09 11:58:04 +0200

    [FIX] commenting to fix graph issue

--- src/Gargantext/Core/Viz/Graph/Tools.hs
+++ src/Gargantext/Core/Viz/Graph/Tools.hs
@@ -134,8 +134,8 @@ cooc2graphWith' doPartitions distance threshold strength myCooc = do
     bridgeness' = bridgeness (fromIntegral nodesApprox) partitions distanceMap
     -- indices of bridgeness seem to start at 1, while computeConfluences
     -- expects 0-based indexing.
-    ks = map (\(a, b) -> (a-1, b-1)) (Map.keys bridgeness')
-    confluence' = Map.mapKeys (\(a, b) -> (a+1, b+1)) $ BAC.computeConfluences 3 ks True
+    -- ks = map (\(a, b) -> (a-1, b-1)) (Map.keys bridgeness')
+    confluence' = Map.empty -- Map.mapKeys (\(a, b) -> (a+1, b+1)) $ BAC.computeConfluences 3 ks True
                -- confluence (Map.keys bridgeness') 3 True False
   seq bridgeness' $ printDebug "bridgeness OK" ()
   seq confluence' $ printDebug "confluence OK" ()
