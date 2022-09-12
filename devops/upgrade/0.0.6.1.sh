9ea8f1fd80060b8a559ae8db9756ded8d2a06693
Alexandre Delanoë <devel+git@delanoe.org>
2022-09-12 19:11:47 +0200

    [FIX] upgrade script

--- devops/upgrade/0.0.6.1.sh
+++ devops/upgrade/0.0.6.1.sh
@@ -2,8 +2,6 @@
 
 # To be executed at the root of the project
 # To upgrade from 0.0.5.9 to 0.0.6.2
-
-
 sudo apt update
 sudo apt -yy upgrade
 
@@ -11,22 +9,12 @@ sudo sed -i "s/buster/bullseye/g" /etc/apt/sources.list
 sudo apt update
 sudo apt -yy dist-upgrade
 
-git pull origin dev
-./bin/install
-
-# Database upgrade
-echo "0.0.6.0 SQL upgrade"
-./bin/psql gargantext.ini < devops/postgres/upgrade/0.0.6.0.sql
-
-echo "Executing script haskell upgrade"
-~/.local/bin/gargantext-upgrade
-
-echo "0.0.6.1 SQL upgrade"
-./bin/psql gargantext.ini < devops/postgres/upgrade/0.0.6.1.sql
-
+##############################################################
+# BACKUP
 sudo -i -u postgres bash << EOF 
 pg_dumpall > /tmp/backup.dump
 EOF
+##############################################################
 
 sudo sed -i "s/bullseye/bookworm/g" /etc/apt/sources.list
 sudo apt update
@@ -35,11 +23,14 @@ sudo apt install -y postgresql-14 libpq-dev
 sudo apt remove --purge postgresql-11 postgresql-13
 sudo apt autoremove
 
-
+##############################################################
+# BACKUP
 sudo -i -u postgres bash << EOF 
 psql < /tmp/backup.dump
 EOF
 
+##############################################################
+# DB CONFIG
 sed -i "s/DB_PORT = 5432/DB_PORT = 5434/" gargantext.ini
 
 # be sure the DB password is the right one
@@ -50,14 +41,25 @@ sudo -i -u postgres psql << EOF
 ALTER ROLE gargantua password '${DBPASS}';
 EOF
 
+##############################################################
+# Make sure compilation is ok
+git pull origin dev
+./bin/install
+
+##############################################################
+# Database upgrade
+echo "0.0.6.0 SQL upgrade"
+./bin/psql gargantext.ini < devops/postgres/upgrade/0.0.6.0.sql
+
+echo "0.0.6.1 SQL upgrade"
+./bin/psql gargantext.ini < devops/postgres/upgrade/0.0.6.1.sql
+
 echo "0.0.6.2 SQL upgrade"
 ./bin/psql gargantext.ini < devops/postgres/upgrade/0.0.6.2.sql
 
-# Make sure compilation is ok
-./bin/install
+echo "Executing script haskell upgrade"
+~/.local/bin/gargantext-upgrade gargantext.ini
 
 echo "Upgrade is over"
-
-
 
 
