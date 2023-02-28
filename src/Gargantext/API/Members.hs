d27f9ebddae23e658452ae29b0e56c60520e8257
Karen Konou <konoukaren@gmail.com>
2023-02-28 14:02:33 +0100

    [API] Team members API for autocomplete

--- src/Gargantext/API/Members.hs
+++ src/Gargantext/API/Members.hs
@@ -9,10 +9,18 @@ import Gargantext.Core.Types (UserId)
 import Gargantext.Database.Admin.Types.Node (NodeType(NodeTeam))
 import Gargantext.Database.Query.Table.Node (getNodesIdWithType)
 import Gargantext.Database.Action.Share (membersOf)
+import Gargantext.Database.Prelude (HasConnectionPool, HasConfig)
+import Gargantext.Core.Mail.Types (HasMail)
+import Control.Monad.Extra (concatMapM)
 
 type MembersAPI = Get '[JSON] [Text]
 
 members :: UserId -> ServerT MembersAPI (GargM Env GargError)
 members _ = do
+  getMembers
+
+getMembers :: (HasConnectionPool env, HasConfig env, HasMail env) => GargM env GargError [Text]
+getMembers = do
   teamNodeIds <- getNodesIdWithType NodeTeam
-  map fst $ concatMap membersOf teamNodeIds
+  m <- concatMapM membersOf teamNodeIds
+  pure $ map fst m
