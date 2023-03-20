f03d154a88a3243b04dd7504e8a74b0b21313831
Alfredo Di Napoli <alfredo@well-typed.com>
2023-03-20 09:08:34 +0100

    Use igraph 0.10.4

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -3,6 +3,61 @@
 rec {
   inherit pkgs;
   ghc = pkgs.haskell.compiler.ghc8107;
+  igraph_0_10_4 = pkgs.igraph.overrideAttrs (finalAttrs: previousAttrs: {
+    version = "0.10.4";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "igraph";
+      repo = "igraph";
+      rev = "0.10.4";
+      hash = "sha256-LsTOxUktGZcp46Ec9QH3+9C+VADMYTZZCjKF1gp36xk=";
+    };
+
+    postPatch = ''
+      echo "0.10.4" > IGRAPH_VERSION
+    '';
+
+    outputs = [ "out" "doc" ];
+
+    buildInputs = [
+      pkgs.arpack
+      pkgs.blas
+      pkgs.glpk
+      pkgs.gmp
+      pkgs.lapack
+      pkgs.libxml2
+      pkgs.plfit
+    ] ++ pkgs.lib.optionals pkgs.stdenv.cc.isClang [
+      pkgs.llvmPackages.openmp
+    ];
+
+    cmakeFlags = [
+      "-DIGRAPH_USE_INTERNAL_BLAS=OFF"
+      "-DIGRAPH_USE_INTERNAL_LAPACK=OFF"
+      "-DIGRAPH_USE_INTERNAL_ARPACK=OFF"
+      "-DIGRAPH_USE_INTERNAL_GLPK=OFF"
+      "-DIGRAPH_USE_INTERNAL_GMP=OFF"
+      "-DIGRAPH_USE_INTERNAL_PLFIT=OFF"
+      "-DIGRAPH_GLPK_SUPPORT=ON"
+      "-DIGRAPH_GRAPHML_SUPPORT=ON"
+      "-DIGRAPH_OPENMP_SUPPORT=ON"
+      "-DIGRAPH_ENABLE_LTO=AUTO"
+      "-DIGRAPH_ENABLE_TLS=ON"
+      "-DBUILD_SHARED_LIBS=ON"
+    ];
+
+    postInstall = ''
+      mkdir -p "$out/share"
+      cp -r doc "$out/share"
+    '';
+
+    postFixup = previousAttrs.postFixup + ''
+      CUR_DIR=$PWD
+      cd "$out/include/igraph" && cp *.h ../
+      cd $CUR_DIR
+    '';
+
+  });
   hsBuildInputs = [
     ghc
     pkgs.cabal-install
@@ -16,7 +71,7 @@ rec {
     gsl
     #haskell-language-server
     hlint
-    igraph
+    igraph_0_10_4
     libffi
     liblapack
     lzma
